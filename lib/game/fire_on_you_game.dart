ba820113a50534968a628ed4c0dfc6b30f3b84b5
Mustafa Yavuz Okumuş <47111280+myavuzokumus@users.noreply.github.com>
2024-08-09 17:02:03 +0300

    Lives added.

--- lib/game/fire_on_you_game.dart
+++ lib/game/fire_on_you_game.dart
@@ -1,18 +1,21 @@
+import 'dart:math';
+import 'dart:async' as timer;
+import 'package:fire_on_you/game/model/fire_type.dart';
+import 'package:fire_on_you/menu/game_over_menu.dart';
 import 'package:flame/components.dart';
 import 'package:flame/events.dart';
 import 'package:flame/game.dart';
 import 'package:flame/input.dart';
 import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
-import '../menu/level_complete_menu.dart';
-import 'level.dart';
+import 'fire_component.dart';
 
 class FireOnYouGame extends FlameGame with TapDetector {
-  late List<Level> levels;
-  int currentLevel = 0;
-  late List<SpriteComponent> fires;
+  late List<FireComponent> fires;
   int score = 0;
+  int lives = 3;
   late AudioPlayer audioPlayer;
+  Random random = Random();
   final BuildContext context;
 
   FireOnYouGame(this.context);
@@ -20,27 +23,32 @@ class FireOnYouGame extends FlameGame with TapDetector {
   @override
   Future<void> onLoad() async {
     super.onLoad();
-    levels = Level.getLevels();
     audioPlayer = AudioPlayer();
-    await loadLevel(currentLevel);
+    fires = [];
+    spawnFires();
   }
 
-  Future<void> loadLevel(int levelIndex) async {
-    fires = [];
-    for (var position in levels[levelIndex].firePositions) {
-      final fire = SpriteComponent()
-        ..sprite = await loadSprite('fire.png')
-        ..size = Vector2(64, 64)
-        ..position = position;
+  void spawnFires() {
+    timer.Timer.periodic(Duration(seconds: random.nextInt(3) + 1), (timer) {
+      if (lives <= 0) {
+        timer.cancel();
+        return;
+      }
+      final position = Vector2(
+        random.nextDouble() * (size.x - 64),
+        random.nextDouble() * (size.y - 64),
+      );
+      final fireInfo = getRandomFireInfo(); // Yangın türünü ve şiddetini al
+      final fire = FireComponent(position: position, timeLimit: random.nextInt(5) + 3, fireInfo: fireInfo);
       add(fire);
       fires.add(fire);
-    }
+    });
   }
 
   @override
   void render(Canvas canvas) {
     super.render(canvas);
-    // Puanı ekranda göster
+    // Puanı ve hakları ekranda göster
     TextPaint tp = TextPaint(
       style: const TextStyle(
         color: Colors.white,
@@ -48,35 +56,54 @@ class FireOnYouGame extends FlameGame with TapDetector {
       ),
     );
     tp.render(canvas, 'Score: $score', Vector2(10, 10));
+    tp.render(canvas, 'Lives: $lives', Vector2(10, 40));
   }
 
   @override
   void update(double dt) {
     super.update(dt);
-    if (fires.isEmpty) {
-      // Tüm yangınlar söndürüldü, bir sonraki seviyeye geç
-      currentLevel = (currentLevel + 1) % levels.length;
+    // Eğer haklar biterse oyun sona erecek
+    if (lives <= 0) {
       pauseEngine();
-      Navigator.push(
-        context,
-        MaterialPageRoute(
-          builder: (context) => LevelCompleteMenu(game: this, score: score),
-        ),
-      );
+      showGameOverScreen();
     }
+  }
+
+  void showGameOverScreen() {
+    // Oyun bittiğinde skorun gösterileceği ekran
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => GameOverMenu(game: this, score: score),
+      ),
+    );
   }
 
   @override
   void onTapDown(TapDownInfo info) {
     for (var fire in fires) {
       if (fire.containsPoint(info.eventPosition.global)) {
-        fire.removeFromParent();
-        fires.remove(fire);
-        score += 100; // Yangın söndürme puanı
-        // Ses efekti oynat
-        audioPlayer.play(AssetSource('sounds/fire_extinguish.wav'));
+        fire.extinguish();
+        if (fire.fireInfo.intensity <= 0) {
+          score += 100;
+          audioPlayer.play(AssetSource('sounds/fire_extinguish.wav'));
+        }
         break;
       }
     }
+  }
+
+  void decreaseLife() {
+    lives--;
+  }
+
+  // Oyunu sıfırla
+  void resetGame() {
+    score = 0;
+    lives = 3;
+    fires.forEach((fire) => fire.removeFromParent());
+    fires.clear();
+    resumeEngine();
+    spawnFires();
   }
 }
